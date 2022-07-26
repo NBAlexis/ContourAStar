80a7eaddfd387c6142690088f1283e7836f3e2c9
Ji-Chong Yang <nbalexis@gmail.com>
2022-07-27 02:13:08 +0800

    add test

--- Applications/Test.py
+++ Applications/Test.py
@@ -23,7 +23,7 @@ from Contour4D.ExtendPath4D import ExtendPath4D
 from Contour4D.Integrand4D import Integrand4D
 from Contour4D.Integrator4D import SparseGridIntegrator4D
 from Contour4D.Integrator4DV2 import SparseGridIntegrator4DV2
-from MathematicaIntegrator.MathLinkIntegrator import MathLinkIntegrator3D
+# from MathematicaIntegrator.MathLinkIntegrator import MathLinkIntegrator3D
 
 
 def intfuncA(x: complex, y: complex, z: complex, w: complex) -> complex:
@@ -31,37 +31,46 @@ def intfuncA(x: complex, y: complex, z: complex, w: complex) -> complex:
 
 
 def intfunc(x: complex, y: complex, z: complex, w: complex) -> complex:
-    return (1 - x) * (1 - x) * (1 - y) / ((0.2 - 0.142857 * x * (1 - x) * y
-                                           - x * (1 - x) * (1 - y) * z
-                                           - 0.142857 * (1 - x) * (1 - x) * y * (1 - y) * z
-                                           + 0.4 * (1 - x) * (1 - x) * y * (1 - y) * (1 - z)
-                                           + w * w * x
-                                           - w * w * y
-                                           + z * z * w
-                                           - w) ** 2)
+    s = 1
+    m1sq = 0.142857
+    return (128 * (-1 + x + z - x * z)) / (
+            (-13 + z ** 2 + x * (-1 + y) * (3 + z ** 2) - y * (3 + z ** 2))
+            * (s * (-1 + x) * (83 + 69 * x + 50 * y + 54 * x * y - 5 * y ** 2 + 5 * x * y ** 2
+                               - (-1 + y) * (-13 - 3 * x + 3 * (-1 + x) * y) * z
+                               - (-1 + y) * (-7 - x + (-1 + x) * y) * z ** 2
+                               - (-1 + x) * (-1 + y) ** 2 * z ** 3
+                               + w ** 2 * (-1 + y) * (-1 + z) ** 2 * (7 + x + y - x * y + (-1 + x) * (-1 + y) * z)
+                               - 4 * w * (-1 + y) * (1 + 3 * x + (-1 + x) * y) * (-1 + z ** 2))
+               + 4 * m1sq * (-7 + z + x * (-1 + y) * (1 + z) - y * (1 + z))
+               * (-13 + z ** 2 + x * (-1 + y) * (3 + z ** 2) - y * (3 + z ** 2))))
 
 
 def intfuncd(x: complex, y: complex, z: complex, w: complex) -> complex:
-    return 0.2 - 0.142857 * x * (1 - x) * y \
-           - x * (1 - x) * (1 - y) * z \
-           - 0.142857 * (1 - x) * (1 - x) * y * (1 - y) * z \
-           + 0.4 * (1 - x) * (1 - x) * y * (1 - y) * (1 - z) \
-           + w * w * x \
-           - w * w * y \
-           + z * z * w \
-           - w
+    s = 1
+    m1sq = 0.142857
+    return (
+            (-13 + z ** 2 + x * (-1 + y) * (3 + z ** 2) - y * (3 + z ** 2))
+            * (s * (-1 + x) * (83 + 69 * x + 50 * y + 54 * x * y - 5 * y ** 2 + 5 * x * y ** 2
+                               - (-1 + y) * (-13 - 3 * x + 3 * (-1 + x) * y) * z
+                               - (-1 + y) * (-7 - x + (-1 + x) * y) * z ** 2
+                               - (-1 + x) * (-1 + y) ** 2 * z ** 3
+                               + w ** 2 * (-1 + y) * (-1 + z) ** 2 * (7 + x + y - x * y + (-1 + x) * (-1 + y) * z)
+                               - 4 * w * (-1 + y) * (1 + 3 * x + (-1 + x) * y) * (-1 + z ** 2))
+               + 4 * m1sq * (-7 + z + x * (-1 + y) * (1 + z) - y * (1 + z))
+               * (-13 + z ** 2 + x * (-1 + y) * (3 + z ** 2) - y * (3 + z ** 2))))
 
 
 # """
 
-intgrand = IntegrandV2(intfunc, None, """(1 - x) * (1 - x) * (1 - y) / ((0.2 - 0.142857 * x * (1 - x) * y
-                                           - x * (1 - x) * (1 - y) * z
-                                           - 0.142857 * (1 - x) * (1 - x) * y * (1 - y) * z
-                                           + 0.4 * (1 - x) * (1 - x) * y * (1 - y) * (1 - z)
-                                           + w * w * x
-                                           - w * w * y
-                                           + z * z * w
-                                           - w) ^ 2)""", "f[(w+1)/2,(w+1)/2,(w+1)/2,(w+1)/2]/16")
+intgrand = IntegrandV2(intfunc, None, """(128*(-1 + x + z - x*z))/((-13 + z^2 + x*(-1 + y)*(3 + z^2) - 
+     y*(3 + z^2))*((-1 + x)*(83 + 69*x + 50*y + 54*x*y - 5*y^2 + 
+        5*x*y^2 - (-1 + y)*(-13 - 3*x + 3*(-1 + x)*y)*
+         z - (-1 + y)*(-7 - x + (-1 + x)*y)*z^2 - (-1 + x)*(-1 + y)^2*
+         z^3 + w^2*(-1 + y)*(-1 + z)^2*(7 + x + y - 
+           x*y + (-1 + x)*(-1 + y)*z) - 
+        4*w*(-1 + y)*(1 + 3*x + (-1 + x)*y)*(-1 + z^2)) + 
+     4*(1/7)*(-7 + z + x*(-1 + y)*(1 + z) - y*(1 + z))*(-13 + z^2 + 
+        x*(-1 + y)*(3 + z^2) - y*(3 + z^2))))""", "f[x,y,z,w]")
 
 # intgrand = Integrand3D(intfunc, 0, 1, 0, 1, 0, 1)
 # intgrand.SetMathematicaExpress("""1 / ((0.2 + 0.1 * x + 0.2 * y + 0.3 * z
@@ -74,13 +83,13 @@ intgrand = IntegrandV2(intfunc, None, """(1 - x) * (1 - x) * (1 - y) / ((0.2 - 0
 #                  + 0.1 * x * x * y * z - 0.2 * x * x * y * y + 0.3 * x * x * z * z
 #                  - 0.3 * y * y * x * z - 0.2 * y * y * z * z + 0.1 * z * z * y * x) ^ 2)""")
 # intgrator = MathLinkIntegrator3D()
-intgrator = SparseGridIntegrator4DV2(epsilon=1.0e-2, logLevel=LogLevel.Warning)
+intgrator = SparseGridIntegrator4DV2(epsilon=1.0e-1, logLevel=LogLevel.General)
 # print(intgrator.Integrate(intgrand, -1 - 1j, 1 + 1j, -1 - 1j, 1 + 1j, -1 - 1j, 1 + 1j))
 # print(intgrator.IntegrateYZ(intgrand, -1 - 1j, -1 - 1j, 1 + 1j, -1 - 1j, 1 + 1j))
 # print(intgrator.IntegrateZ(intgrand, -1 - 1j, 1 + 1j, -1 - 1j, 1 + 1j))
 
 # grid3d = ExtendPath3D(11, 11, 0, intgrator, intgrand)
-grid4d = ExtendPath4D(7, 7, 0, intgrator, intgrand)
+grid4d = ExtendPath4D(9, 9, 0, intgrator, intgrand)
 # grid3d = CMarkedGrids3D(5, 5, 1, intgrator, intgrand, requireEdge=True)
 print(grid4d.Integrate())
 print(grid4d.GatherInfo())
